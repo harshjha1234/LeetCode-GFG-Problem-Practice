cb2fcdd412cbb14d5a19ff71b4ac3fc6d7446d21
Harsh Kumar <rajharsh5119@gmail.com>
2023-02-24 23:56:48 +0530

    109. Convert Sorted List to Binary Search Tree
    109. Convert Sorted List to Binary Search Tree
    
    Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height-balanced binary search tree.
    
     
    
    Example 1:
    
    
    Input: head = [-10,-3,0,5,9]
    Output: [0,-3,9,-10,null,5]
    Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
    Example 2:
    
    Input: head = []
    Output: []
     
    
    Constraints:
    
    The number of nodes in head is in the range [0, 2 * 104].
    -10^5 <= Node.val <= 10^5

---
+++ Convert Sorted List To Binary Search Tree/Convert Sorted List to Binary Search Tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> l = new ArrayList<>();
+        while (head != null) {
+            l.add(head.val);
+            head = head.next;
+        }
+        return BST(l, 0, l.size() - 1);
+    }
+    TreeNode BST(List<Integer> list, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = BST(list, left, mid - 1);
+        root.right = BST(list, mid + 1, right);
+        return root;
+    }
+}
