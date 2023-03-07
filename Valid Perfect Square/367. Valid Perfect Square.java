5ec1be25cf1dcaa20006606088ed715d7e7ca018
Harsh Kumar <rajharsh5119@gmail.com>
2023-03-07 23:17:05 +0530

    Create 367. Valid Perfect Square.java

---
+++ Valid Perfect Square/367. Valid Perfect Square.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       if(num <= 0) {
+           return false;
+       }
+       int left = 1, right = num;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(mid > num / mid){
+                right = mid - 1;
+            }else if(mid < num / mid){
+                left = mid + 1;
+            }else{
+                return num % mid == 0;
+            }
+        }
+        return false; 
+    }
+}
