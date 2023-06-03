927dc5b4942252f73e58e5bd95b36b6d9fa064f8
Harsh Kumar <rajharsh5119@gmail.com>
2023-06-03 22:37:14 +0530

    Create Lexicographical numbers.java

---
+++ Lexicographical numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public List<Integer> lexicalOrder(int n) {
+
+      List<Integer> list = new ArrayList<>();
+
+        String[] str = new String[n];
+
+        for(int i=1;i<=n;i++) str[i-1] = Integer.toString(i);
+
+        Arrays.sort(str);
+
+        for(String s: str) list.add(Integer.parseInt(s));
+
+        return list;  
+
+    }
+
+}
