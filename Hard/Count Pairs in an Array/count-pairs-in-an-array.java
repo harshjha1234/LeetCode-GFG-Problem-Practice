65f3bfd00d07f09e0aa955b718cb1244f368b1fd
Harsh Kumar <rajharsh5119@gmail.com>
2024-02-05 22:12:30 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Count Pairs in an Array/count-pairs-in-an-array.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+{
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();  
+            System.out.println(obj.countPairs(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution { 
+    private static int mergeAndCount(int[] arr, int l, int m, int r){
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+        int i = 0, j = 0, k = l, swaps = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
+    }
+    private static int mergeSortAndCount(int[] arr, int l, int r){
+        int count = 0;
+        if (l < r) {
+            int m = (l + r) / 2;
+            count += mergeSortAndCount(arr, l, m);
+            count += mergeSortAndCount(arr, m + 1, r);
+            count += mergeAndCount(arr, l, m, r);
+        }
+        return count;
+    }
+    static int countPairs(int arr[], int n) 
+    {
+        // Your code goes here
+        for(int i=0; i<n; i++){
+           arr[i] *= i;
+        }
+       return mergeSortAndCount(arr, 0, n-1);
+    }
+}
