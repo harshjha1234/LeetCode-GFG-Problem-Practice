663cd776bb6f21d84e802225507caf059d6d4a59
Harsh Kumar <rajharsh5119@gmail.com>
2024-03-20 11:18:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if Tree is Isomorphic/check-if-tree-is-isomorphic.java
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s1 = br.readLine();
+	            String s2 = br.readLine();
+    	    	Node root1 = buildTree(s1);
+    	    	Node root2 = buildTree(s2);
+        	    Solution tree = new Solution();
+        	    if (tree.isIsomorphic(root1, root2) == true) 
+                    System.out.println("Yes"); 
+                else
+                    System.out.println("No");
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution  
+{ 
+    // Return True if the given trees are isomotphic. Else return False.
+    void invert(Node root){
+        if(root == null || root.left == null && root.right == null)return;
+        invert(root.left);
+        invert(root.right);
+        Node l = root.left;
+        Node r = root.right;
+        root.left = r;
+        root.right = l;
+    }
+    boolean isSame(Node p,Node q){
+        if(p == null && q == null)return true;
+        if(p == null || q == null)return false;
+        boolean op1 = isSame(p.left,q.left);
+        boolean op2 = isSame(p.right,q.right);
+        boolean op3 = p.data == q.data;
+        if(op1 && op2 && op3)return true;
+        else return false;
+    }
+    boolean isIsomorphic(Node root1, Node root2)  
+    { 
+         // code here.
+         if(isSame(root1,root2))return true;
+         invert(root1);
+         return isSame(root1,root2);
+    }
+    
+}    
